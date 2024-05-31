37a73099e52b63552fbe1a703d1dbd235d54cf6d
adelmonte <138840776+adelmonte@users.noreply.github.com>
2024-05-30 22:47:01 -0700

    Update performance_optimized.sh

--- performance_optimized.sh
+++ performance_optimized.sh
@@ -5,16 +5,12 @@ truncate -s 0 /etc/sysctl.d/99-kernel-optimized.conf
 
 # CPU Scheduler and Frequency Scaling
 echo "kernel.sched_autogroup_enabled=1" >> /etc/sysctl.d/99-kernel-optimized.conf
-echo "kernel.sched_migration_cost_ns=500000" >> /etc/sysctl.d/99-kernel-optimized.conf
-echo "kernel.sched_min_granularity_ns=1000000" >> /etc/sysctl.d/99-kernel-optimized.conf
-echo "kernel.sched_wakeup_granularity_ns=2000000" >> /etc/sysctl.d/99-kernel-optimized.conf
 echo "kernel.sched_features=0x04,~0x20" >> /etc/sysctl.d/99-kernel-optimized.conf
 
 # Virtual Memory and Disk I/O
 echo "vm.dirty_ratio=5" >> /etc/sysctl.d/99-kernel-optimized.conf
 echo "vm.dirty_background_ratio=3" >> /etc/sysctl.d/99-kernel-optimized.conf
-echo "vm.swappiness=1" >> /etc/sysctl.d/99-kernel-optimized.conf
-echo "vm.vfs_cache_pressure=100" >> /etc/sysctl.d/99-kernel-optimized.conf
+echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.d/99-kernel-optimized.conf
 echo "vm.dirty_writeback_centisecs=3000" >> /etc/sysctl.d/99-kernel-optimized.conf
 
 # Network kernel
@@ -27,13 +23,6 @@ echo "kernel.preempt_max_cpu_resv_pct=50" >> /etc/sysctl.d/99-kernel-optimized.c
 
 # Laptop Mode
 echo "vm.laptop_mode=0" >> /etc/sysctl.d/99-kernel-optimized.conf
-
-# CPU Frequency Scaling
-echo "kernel.sched_cpufreq_util_plugin=0" >> /etc/sysctl.d/99-kernel-optimized.conf
-echo "kernel.sched_cpufreq_elc.llpml=0" >> /etc/sysctl.d/99-kernel-optimized.conf
-echo "kernel.sched_cpufreq_elc.pm_qos_no_authority=0" >> /etc/sysctl.d/99-kernel-optimized.conf
-echo "kernel.sched_cpufreq_util_data.cpu_util_sum_allowed_cpu_buck_ratio=1" >> /etc/sysctl.d/99-kernel-optimized.conf
-echo "kernel.nmi_watchdog=1" >> /etc/sysctl.d/99-kernel-optimized.conf
 
 # Apply the changes
 sysctl --system
