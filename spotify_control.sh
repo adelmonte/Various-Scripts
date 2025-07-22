89a128421db6fffae9caf720551fd9fab520914d
adelmonte <138840776+adelmonte@users.noreply.github.com>
2025-07-22 11:46:40 -0400

    Update spotify_control.sh

--- spotify_control.sh
+++ spotify_control.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Spotify Media Control Script
 # Usage: ./spotify_control.sh [pause|next|previous|volup|voldown|play|stop]
 
@@ -11,42 +10,29 @@ spotify_dbus() {
         org.mpris.MediaPlayer2.Player.$1
 }
 
+get_volume() {
+    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Volume 2>/dev/null | grep -oP 'double \K[0-9.]+'
+}
+
+set_volume() {
+    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Set string:org.mpris.MediaPlayer2.Player string:Volume variant:double:$1
+}
+
 case "$1" in
-    "pause")
-        spotify_dbus PlayPause
-        ;;
-    "play")
-        spotify_dbus Play
-        ;;
-    "stop")
-        spotify_dbus Stop
-        ;;
-    "next")
-        spotify_dbus Next
-        ;;
-    "previous")
-        spotify_dbus Previous
-        ;;
+    "pause") spotify_dbus PlayPause ;;
+    "play") spotify_dbus Play ;;
+    "stop") spotify_dbus Stop ;;
+    "next") spotify_dbus Next ;;
+    "previous") spotify_dbus Previous ;;
     "volup")
-        # Get Spotify's sink input and increase its volume
-        spotify_sink=$(pactl list sink-inputs | grep -B 20 "spotify" | grep "Sink Input #" | tail -1 | cut -d'#' -f2)
-        if [ -n "$spotify_sink" ]; then
-            pactl set-sink-input-volume $spotify_sink +5%
-        else
-            echo "Spotify not found in audio streams"
-        fi
+        current=$(get_volume)
+        new=$(echo "$current + 0.05" | bc)
+        set_volume $new
         ;;
     "voldown")
-        # Get Spotify's sink input and decrease its volume
-        spotify_sink=$(pactl list sink-inputs | grep -B 20 "spotify" | grep "Sink Input #" | tail -1 | cut -d'#' -f2)
-        if [ -n "$spotify_sink" ]; then
-            pactl set-sink-input-volume $spotify_sink -5%
-        else
-            echo "Spotify not found in audio streams"
-        fi
+        current=$(get_volume)
+        new=$(echo "$current - 0.05" | bc)
+        set_volume $new
         ;;
-    *)
-        echo "Usage: $0 [pause|play|stop|next|previous|volup|voldown]"
-        exit 1
-        ;;
+    *) echo "Usage: $0 [pause|play|stop|next|previous|volup|voldown]" ;;
 esac
